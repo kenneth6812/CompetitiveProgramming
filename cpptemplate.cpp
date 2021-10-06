95801153f6c45f3fa0ce826c2885ed2d1cb37e76
kenJPG <92031860+kenJPG@users.noreply.github.com>
2021-10-06 20:20:19 +0800

    Add files via upload

---
+++ cpptemplate.cpp
@@ -0,0 +1,13 @@
+#pragma GCC optimize("O3")
+#pragma GCC target("popcnt")
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define all(x) x.begin(), x.end()
+#define chmax(x, a) x = max(x, a);
+#define chmin(x, a) x = min(x, a);
+
+int main() {
+    FASTIO;
+    
+}
